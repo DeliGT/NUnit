552aee885f22de667878bd55d6fe9fd67118bfb4
DeliGT <takis20100@student.hamk.fi>
2021-10-14 19:52:35 +0300

    FizzBuzz Testi

--- ConsoleA_Test/UnitTest1.cs
+++ ConsoleA_Test/UnitTest1.cs
@@ -6,9 +6,11 @@ namespace ConsoleA_Test
     [TestFixture]
     public class Tests
     {
+        FizzBuzz buzz = null;
         [SetUp]
         public void Setup()
         {
+            buzz = new FizzBuzz();
         }
 
         [Test]
@@ -17,6 +19,26 @@ namespace ConsoleA_Test
             Laskin OmaLaskin = new Laskin();
             int summa = OmaLaskin.LaskeSumma(20, 4);
             Assert.AreEqual(24, summa);
+        }
+        [Test]
+        public void FizzBuzzTesti()
+        {
+            Assert.AreEqual(buzz.DoFizzBuzz(1), "1");
+        }
+        [Test]
+        public void FizzBuzzTesti1()
+        {
+            Assert.AreEqual(buzz.DoFizzBuzz(3), "Fizz");            
+        }
+        [Test]
+        public void FizzBuzzTesti2()
+        {
+            Assert.AreSame(buzz.DoFizzBuzz(5), "Buzz");
+        }
+        [Test]
+        public void FizzBuzzTesti3()
+        {
+            Assert.AreSame(buzz.DoFizzBuzz(15), "FizzBuzz");
         }
     }
 }
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleA_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz Harjoitus");
+            Console.WriteLine("Anna luku 1 - 100 välillä:");
+            var buzz = new FizzBuzz();
+            Console.WriteLine(buzz.DoFizzBuzz(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+    public class FizzBuzz
+    {
+        public string DoFizzBuzz(int luku)
+        {
+ 
+            bool fizz = luku % 3 == 0;
+            bool buzz = luku % 5 == 0;
+            if (fizz && buzz)
+                return "FizzBuzz";
+            else if (fizz)
+                return "Fizz";
+            else if (buzz)
+                return "Buzz";
+            else
+                return luku.ToString();
+            
+        }
+    }
+}
